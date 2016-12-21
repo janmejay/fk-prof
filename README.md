8c00aa6c9c5f806004ad3edf60da90ad425e42fb
Janmejay Singh <singh.janmejay@gmail.com>
2016-12-21 15:35:36 +0530

    e2e test module + some other cleanup

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-honest-profiler
-===============
+fk-prof
+=======
 
-An usable and honest profiler for the JVM. For documentation please refer to
-[The Wiki](https://github.com/RichardWarburton/honest-profiler/wiki)
-
-* [Download the Binary](http://insightfullogic.com/honest-profiler.zip)
-* [How to Build](https://github.com/RichardWarburton/honest-profiler/wiki/How-to-build)
+Build instructions:
+$ (cd recorder && cmake CMakeLists.txt)
+$ mvn clean test
--- recorder/src/test/java/fk/prof/utils/AgentRunner.java
+++ recorder/src/test/java/fk/prof/utils/AgentRunner.java
@@ -1,16 +1,16 @@
 /**
  * Copyright (c) 2014 Richard Warburton (richard.warburton@gmail.com)
- * <p>
+ * <p/>
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction, including without limitation
  * the rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
- * <p>
+ * <p/>
  * The above copyright notice and this permission notice shall be included
  * in all copies or substantial portions of the Software.
- * <p>
+ * <p/>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
  * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,7 +22,6 @@
 package fk.prof.utils;
 
 import fk.prof.Platforms;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,9 +30,7 @@ import java.util.function.Consumer;
 
 import static java.lang.Integer.parseInt;
 
-public class AgentRunner
-{
-
+public class AgentRunner {
     private static final Logger logger = LoggerFactory.getLogger(AgentRunner.class);
     public static final String DEFAULT_AGENT_INTERVAL = "interval=100";
 
@@ -43,106 +40,84 @@ public class AgentRunner
     private Process process;
     private int processId;
 
-    private AgentRunner(final String className, final String args)
-    {
+    private AgentRunner(final String className, final String args) {
         this.className = className;
         this.args = args;
     }
 
-    public static void run(final String className, final Consumer<AgentRunner> handler) throws IOException
-    {
+    public static void run(final String className, final Consumer<AgentRunner> handler) throws IOException {
         run(className, (String) null, handler);
     }
 
     public static void run(final String className,
                            final String[] args,
-                           final Consumer<AgentRunner> handler) throws IOException
-    {
+                           final Consumer<AgentRunner> handler) throws IOException {
         run(className, String.join(",", args), handler);
     }
 
     public static void run(final String className,
                            final String args,
-                           final Consumer<AgentRunner> handler) throws IOException
-    {
+                           final Consumer<AgentRunner> handler) throws IOException {
         AgentRunner runner = new AgentRunner(className, args);
         runner.start();
-        try
-        {
+        try {
             handler.accept(runner);
-        }
-        finally
-        {
+        } finally {
             runner.stop();
         }
     }
 
-    private void start() throws IOException
-    {
+    private void start() throws IOException {
         startProcess();
         readProcessId();
     }
 
-    private void startProcess() throws IOException
-    {
+    private void startProcess() throws IOException {
         String java = System.getProperty("java.home") + "/bin/java";
         String agentArg = "-agentpath:build/liblagent" + Platforms.getDynamicLibraryExtension() + (args != null ? "=" + args : "");
         // Eg: java -agentpath:build/liblagent.so -cp target/classes/ InfiniteExample
         process = new ProcessBuilder()
-            .command(java, agentArg, "-cp", "target/classes/", className)
-            .redirectError(new File("/tmp/error.log"))
-            .start();
+                .command(java, agentArg, "-cp", "target/classes/", className)
+                .redirectError(new File("/tmp/error.log"))
+                .start();
     }
 
-    private void readProcessId() throws IOException
-    {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream())))
-        {
+    private void readProcessId() throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
             String line = reader.readLine();
             System.out.println(line);
             processId = parseInt(line);
         }
     }
 
-    private void stop()
-    {
+    private void stop() {
         process.destroy();
-        try
-        {
+        try {
             process.waitFor();
-        }
-        catch (InterruptedException e)
-        {
+        } catch (InterruptedException e) {
             logger.info(e.getMessage(), e);
         }
     }
 
-    public int getProcessId()
-    {
+    public int getProcessId() {
         return processId;
     }
 
-    public void startProfiler()
-    {
+    public void startProfiler() {
         messageProcess('S');
     }
 
-    public void stopProfiler()
-    {
+    public void stopProfiler() {
         messageProcess('s');
     }
 
-    private void messageProcess(final char message)
-    {
-        try
-        {
+    private void messageProcess(final char message) {
+        try {
             final OutputStream outputStream = process.getOutputStream();
             outputStream.write(message);
             outputStream.write('\n');
             outputStream.flush();
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
