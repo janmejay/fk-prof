abd2539722ba9df5558c9b626d593b8b6489498d
fk-ajain <ankurjain.v@darthvader.local>
2016-12-27 19:41:03 +0530

    in middle of some changes

--- common/src/main/java/fk/prof/common/Utils.java
+++ common/src/main/java/fk/prof/common/Utils.java
@@ -2,12 +2,22 @@ package fk.prof.common;
 
 import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
+import com.google.common.primitives.Longs;
+import com.google.common.primitives.UnsignedInteger;
+import com.google.common.primitives.UnsignedInts;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 public class Utils {
     private static HashFunction murmur3_32 = Hashing.murmur3_32();
 
     public static HashFunction getHashFunctionForMurmur3_32() {
         return murmur3_32;
+    }
+
+    public byte[] unsignedIntToByteArray(long value) {
+        return Longs.toByteArray(value);
     }
 
 }
