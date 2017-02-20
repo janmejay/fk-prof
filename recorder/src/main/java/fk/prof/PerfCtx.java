3a0602f40e87cf6c4378f6ab78230b285e46b0e4
Janmejay Singh <singh.janmejay@gmail.com>
2017-02-20 20:17:13 +0530

    Fixed some more documentation related concerns called out in PR#32.

--- recorder/src/main/java/fk/prof/PerfCtx.java
+++ recorder/src/main/java/fk/prof/PerfCtx.java
@@ -6,16 +6,13 @@ package fk.prof;
  * Note about constructors:
  * Creates a perf-data-recording-context with desired merge semantics. This internally checks if a conflicting duplicate context (same name, but different properties) are created with conflicting coverage-pct and/or merge-semantics) creation is being attempted and fails the call.
  * <p>
- * So the best way to use this is to create a static+final instance and use it in the relevant places. If that is not possible, separate instances may be used, but user must take care to not provide conflicting values for these parameters. Also, constructor here has non-trivial performance-cost, so its best used as static+final instance.
+ * The best way to use this is to create a static+final instance and use it in the relevant places. If that is not possible, separate instances may be used, but user must take care to not provide conflicting values for these parameters. Also, constructor here has a minor performance-cost, so its best used as static+final instance.
  * <p>
  * Constraints / Limits:
- * 1. An application (single JVM instance) can have only upto 100 unique PerfCtx instances (uniqueness is implied by name)
+ * 1. An application (single JVM instance) can have only upto 200 unique PerfCtx instances (uniqueness is implied by name)
  * 2. There exists an upper-limit to number of context to be tracked. This may change between releases. Consult the package-maintainers to learn the actual enforced limit.
- * 3. No more than 6 perf-ctx instances should be nested with "scoped" merge-semantic.
- * 
- * Internal technical limitation:
- * Purely technically, it is possible to track 100 statically created perf-ctx with upto 6 level depth using prime-numbers to represent each statically created ctx so multiplication of prime-numbers can be used to represent to scope with a unsigned 64-bit field being used to represent the ctx-id (along with merge-semantic (2 bits) and coverage-pct (7 bits), which is total of 9 bits overhead, leaving us a max width of 55 bits and 2^55 > 541^6, (541 is the 100th prime number), but it doesn't seem like anyone should need more than 64 contexts (in totality) except for when context nesting is buggy (not correctly done).
- */
+ * 3. No more than 5 perf-ctx instances should be nested in general. Nesting with the same ctx is the only exception (except for scoped-strict) because no additional context tracking is necessary in this case.
+   */
 public class PerfCtx {
     private static boolean noDepMode = false; 
     private final long ctxId;
@@ -31,7 +28,7 @@ public class PerfCtx {
     }
 
     public PerfCtx(final String name, final int coveragePct, final MergeSemantics mrgSem) throws IllegalArgumentException, PerfCtxInitException {
-        if (coveragePct < 0 || coveragePct > 100) {
+        if (coveragePct < 0 || coveragePct > 100) {//coverage == 0 is actually not invalid, because one can write scoped CTXs, which don't make sense in isolation
             throw new IllegalArgumentException(String.format("Coverage-percentage value %s is not valid, a valid value must be in the range [0, 100].", coveragePct));
         }
         if (name.contains("%") || name.contains("~") || name.contains("<") || name.contains(">")) {
