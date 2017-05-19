8d860312b59ca06e0278a21cf0e26a7a365aba4e
fk-ajain <ankurjain.v@flipkart.com>
2017-05-19 18:04:31 +0530

    PoC of rendering list of stack lines using react virtualised

--- ui/app/components/MethodTreeComponent/MethodTreeComponent.js
+++ ui/app/components/MethodTreeComponent/MethodTreeComponent.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import StackTreeElement from 'components/StackTreeElementComponent';
 import { withRouter } from 'react-router';
+import Infinite from 'react-infinite';
+import { List, AutoSizer, WindowScroller } from 'react-virtualized';
 import memoize from 'utils/memoize';
 import debounce from 'utils/debounce';
 
 import styles from './MethodTreeComponent.css';
 import treeStyles from 'components/StackTreeElementComponent/StackTreeElementComponent.css';
 import HotMethodNode from '../../pojos/HotMethodNode';
+import 'react-virtualized/styles.css';
 
 const noop = () => {};
 const filterPaths = (pathSubset, k) => k.indexOf(pathSubset) === 0;
+const data = [];
 
 //Input is expected to be Array of (nodeIndex, callCount)   and
 //output returned is an array of objects of type HotMethodNode
@@ -53,6 +57,10 @@ class MethodTreeComponent extends Component {
       opened: {}, // keeps track of all opened/closed nodes
       highlighted: {}, // keeps track of all highlighted nodes
     };
+    this.renderData = [];
+    this.rowRenderer = this.rowRenderer.bind(this);
+    this.listRef = null;
+    this.setListRef = this.setListRef.bind(this);
     this.userToggledNode = null;
     this.renderTree = this.renderTree.bind(this);
     this.toggle = this.toggle.bind(this);
@@ -245,40 +253,188 @@ class MethodTreeComponent extends Component {
     } else {
       nodeIndexes = this.props.nodeIndexes;
     }
-    const treeNodes = this.renderTree(nodeIndexes, filterText);
-
+    // const treeNodes = this.renderTree(nodeIndexes, filterText);
+    this.setRenderData(nodeIndexes, filterText, '', false, 0, false);
+    console.log("tree nodes calculated from scratch");
     return (
       <div>
-        <div className={treeStyles.container}>
-          <table>
-            <thead><tr>
-              <th className={treeStyles.fixedRightCol1}>Samples</th>
-              <th>
-                <div className={`mdl-textfield mdl-js-textfield ${styles.filterBox}`}>
-                  <label htmlFor="method_filter" style={{fontWeight: "bold"}}>
-                    {nextNodesAccessorField === 'parent' ? "Filter hot methods" : "Filter root callers"}
-                  </label>
-                  <input
-                    className={`mdl-textfield__input`}
-                    type="text"
-                    defaultValue={filterText}
-                    autoFocus
-                    onChange={this.debouncedHandleFilterChange}
-                    id="method_filter"
-                  />
-                </div>
-              </th>
-            </tr></thead>
-            <tbody>
-              {treeNodes}
-            </tbody>
-          </table>
+        <div style={{flex: "1 1 auto", height: "400px"}}>
+          <AutoSizer >
+            {({ width, height }) => (
+              <List
+                ref={this.setListRef}
+                width={width}
+                height={height}
+                rowCount={this.renderData.length}
+                rowHeight={25}
+                rowRenderer={this.rowRenderer}
+                overscanRowCount={2}
+              />
+            )}
+          </AutoSizer>
         </div>
-      {filterText && !treeNodes.length && (
-        <p className={styles.alert}>Sorry, no results found for your search query!</p>
-      )}
+        {filterText && !treeNodes.length && (
+          <p className={styles.alert}>Sorry, no results found for your search query!</p>
+        )}
       </div>
     );
+  }
+
+  rowRenderer (params) {
+    let newstyle = {display: "flex", flexDirection: "row", alignItems: "center"};
+    let rowstyle = Object.assign({}, params.style);
+    let rowdata = this.renderData[params.index];
+    let n = rowdata[1], uniqueId = rowdata[0];
+
+    //TODO: optimize, move below assignment to lifecycle method when properties are received by component
+    const percentageDenominator = (this.props.allNodes.length > 0)? this.props.allNodes[0].onStack: 1;
+
+    let countToDisplay, newNodeIndexes;
+    let displayName = this.props.methodLookup[n.name][0];
+    let displayNameWithArgs = this.props.methodLookup[n.name][0] + this.props.methodLookup[n.name][1];
+
+    //This condition is equivalent to (n instanceOf HotMethodNode)
+    //since nextNodesAccessorField is = parent in hot method view and
+    //Node type for dedupedNodes is HotMethodNode from above
+    if (this.props.nextNodesAccessorField === 'parent') {
+      newNodeIndexes = n.parentsWithSampledCallCount;
+      const lineNoOrNot = (n.belongsToTopLayer)? '' : ':' + n.lineNo;
+      displayName = displayName + lineNoOrNot;
+      displayNameWithArgs = displayNameWithArgs + lineNoOrNot;
+      countToDisplay = n.sampledCallCount;
+    } else {
+      // using the index i because in call tree the name of sibling nodes
+      // can be same, react will throw up, argh!
+      newNodeIndexes = n.children;
+      displayName = displayName + ':' + n.lineNo;
+      displayNameWithArgs = displayNameWithArgs + ':' + n.lineNo;
+      countToDisplay = n.onStack;
+    }
+    const onStackPercentage = Number((countToDisplay * 100) / percentageDenominator).toFixed(2);
+    const isHighlighted = Object.keys(this.state.highlighted)
+      .filter(filterPaths.bind(null, uniqueId));
+
+    return (
+      <div key={params.key} style={rowstyle}>
+         <StackTreeElement
+            nodename={displayNameWithArgs}
+            key={uniqueId}
+            stackline={displayName}
+            samples={countToDisplay}
+            samplesPct={onStackPercentage}
+            indent={rowdata[2]}
+            nodestate={this.state.opened[uniqueId]}
+            highlight={isHighlighted.length}
+            subdued={rowdata[3] == 1 ? true : false}
+            onHighlight={noop}
+            onClick={newNodeIndexes ? this.toggle.bind(this, uniqueId) : noop}>
+          </StackTreeElement>
+      </div>
+    );
+  }
+
+  setRenderData (nodeIndexes = [], filterText, parentPath, parentHasSiblings, parentIndent, autoExpand) {
+    const renderStack = [];
+    renderStack.push({
+      ae: autoExpand, //false, //autoExpand behaviour
+      p_pth: parentPath, // '', //parent path
+      gen: {
+        nis: nodeIndexes, //indexes of first-level nodes in the tree subject to de-duplication
+        p_ind: parentIndent, //0, //indentation of parent node
+        p_sib: parentHasSiblings, //false //parentHasSiblings
+      }
+    });
+
+    const percentageDenominator = (this.props.allNodes.length > 0)? this.props.allNodes[0].onStack: 1;
+    this.renderData = [];
+
+    while(renderStack.length > 0) {
+      let se = renderStack.pop();
+      if (se.gen) {
+        // only need to de-dupe for bottom-up not top-down,
+        // hence the ternary :/
+        //TODO: Check if this memoization can be skipped with just using the isOpen state variable
+        const dedupedNodes = this.props.nextNodesAccessorField === 'parent'
+          ? this.memoizedDedupeNodes(...se.gen.nis)
+          : se.gen.nis.map((nodeIndex) => this.props.allNodes[nodeIndex]).slice().sort((a, b) => b.onStack - a.onStack);
+
+        //Indent should always be zero if no parent
+        //Otherwise, if parent has siblings or if this node has siblings, do a major indent of the nodes, minor indent otherwise
+        const indent = !se.p_pth ? 0 : ((se.gen.p_sib || dedupedNodes.length > 1 ) ? se.gen.p_ind + 10 : se.gen.p_ind + 3);
+        renderStack.push({
+          ae: se.ae,
+          p_pth: se.p_pth,
+          node: {
+            dn: dedupedNodes, //first-level nodes
+            ind: indent, //indentation to be applied to rendered node
+            idx: 0 //index in array "dn" to identify the node to render
+          }
+        });
+      } else {
+        if(se.node.idx >= se.node.dn.length) {
+          continue;
+        }
+        const n = se.node.dn[se.node.idx];
+        //Node has been retrieved so it is safe to increment index and push stack entry back in render stack
+        //Fields from this entry will be read further in this iteration but not modified beyond this point, avoiding un-necessary object copy
+        se.node.idx++;
+        //After index increment, stack entry refers to next sibling, pushing it now itself on stack
+        //This ensures that stack entries of children of the current node are pushed later and hence processed earlier
+        renderStack.push(se);
+
+        let displayName = this.props.methodLookup[n.name][0];
+        //If this is a first-level node(p_pth will be empty) and filter is applied, skip rendering of node if display name does not match the filter
+        if(filterText && !se.p_pth && !displayName.match(new RegExp(filterText, 'i'))) {
+          continue;
+        }
+
+        let uniqueId, newNodeIndexes;
+        //This condition is equivalent to (n instanceOf HotMethodNode)
+        //since nextNodesAccessorField is = parent in hot method view and
+        //Node type for dedupedNodes is HotMethodNode from above
+        if (this.props.nextNodesAccessorField === 'parent') {
+          uniqueId = `${se.p_pth}->${n.identifier()}`;
+          newNodeIndexes = n.parentsWithSampledCallCount;
+        } else {
+          // using the index i because in call tree the name of sibling nodes
+          // can be same, react will throw up, argh!
+          uniqueId = `${se.p_pth}->${n.name}:${n.lineNo}`;
+          newNodeIndexes = n.children;
+        }
+
+        //By default, keep auto expand behavior of children same as parent.
+        //As a special case, if this is the node toggled by user and it was expanded, then enable auto expand in the children of this node
+        let childAutoExpand = se.ae;
+        if(this.userToggledNode && this.userToggledNode == uniqueId && this.state.opened[uniqueId]) {
+          childAutoExpand = true;
+        }
+        // Following condition should always be evaluated after the childAutoExpand is set, since this mutates this.state.opened[uniqueId]
+        // If auto expand behavior is enabled and only single node is being rendered, expand the node
+        // Or if the node has no children, then expand the node, so that expanded icon is rendered against this node
+        if((se.ae && se.node.dn.length == 1) || newNodeIndexes.length == 0) {
+          this.state.opened[uniqueId] = true;
+        }
+
+        const nodeData = [uniqueId, n, se.node.ind, se.node.dn.length, childAutoExpand];
+        this.renderData.push(nodeData);
+
+        if(this.state.opened[uniqueId] && newNodeIndexes) {
+          renderStack.push({
+            ae: childAutoExpand,
+            p_pth: uniqueId,
+            gen: {
+              nis: newNodeIndexes,
+              p_ind: se.node.ind,
+              p_sib: se.node.dn.length > 1
+            }
+          });
+        }
+      }
+    }
+  }
+
+  setListRef (ref) {
+    this.listRef = ref;
   }
 }
 
@@ -291,3 +447,26 @@ MethodTreeComponent.propTypes = {
 };
 
 export default withRouter(MethodTreeComponent);
+
+
+/*
+<WindowScroller>
+            {({ height, isScrolling, scrollTop }) => (
+              <AutoSizer disableHeight>
+                {({ width }) => (
+                  <List
+                    autoHeight
+                    width={width}
+                    height={height}
+                    isScrolling={isScrolling}
+                    scrollTop={scrollTop}
+                    rowCount={this.renderData.length}
+                    rowHeight={25}
+                    rowRenderer={this.rowRenderer}
+                    overscanRowCount={2}
+                  />
+                )}
+              </AutoSizer>
+            )}
+          </WindowScroller>
+          */
--- ui/app/components/StackTreeElementComponent/StackTreeElementComponent.js
+++ ui/app/components/StackTreeElementComponent/StackTreeElementComponent.js
@@ -7,6 +7,28 @@ class StackTreeElementComponent extends Component {
     this.state = {
     };
   }
+  
+  shouldComponentUpdate(nextProps, nextState) {
+    if(this.props.stackline === "java.net.SocketOutputStream.socketWrite0") {
+      console.log("scu", this.props.stackline, nextProps.stackline);
+    }
+    if(this.props.nodestate !== nextProps.nodestate) {
+      if(this.props.stackline === "java.net.SocketOutputStream.socketWrite0") {
+        console.log("scu", true);
+      }
+      return true;
+    }
+    if(this.props.highlight !== nextProps.highlight) {
+      if(this.props.stackline === "java.net.SocketOutputStream.socketWrite0") {
+        console.log("scu", true);
+      }
+      return true;
+    }
+    if(this.props.stackline === "java.net.SocketOutputStream.socketWrite0") {
+      console.log("scu", false);
+    }
+    return false;
+  }
 
   getStyleAndIconForNode() {
     if (this.props.nodestate) {
@@ -21,10 +43,36 @@ class StackTreeElementComponent extends Component {
   }
 
   render () {
+    if(this.props.stackline === "java.net.SocketOutputStream.socketWrite0") {
+      console.log("Sdfsf");
+    }
     let leftPadding = (this.props.indent || 0) + "px";
     return (
-      <tr className={this.props.highlight ? styles.highlight : this.props.subdued && styles.subdued}>
-        <td className={styles.fixedRightCol1}>
+      <div className={this.props.highlight ? styles.highlight : this.props.subdued && styles.subdued}>
+        <div>
+          <div className={styles.stackline} style={{marginLeft: leftPadding}} title={this.props.nodename}>
+            <span className={`material-icons mdl-color-text--primary ${styles.nodeIcon}`} onClick={this.props.onHighlight}>
+              {this.getIconForHighlight()}
+            </span>
+            <span className={styles.stacklineText} onClick={this.props.onClick}>
+              <span className={`material-icons ${this.getStyleAndIconForNode()[0]} ${styles.nodeIcon}`}>
+                {this.getStyleAndIconForNode()[1]}
+              </span>
+              <span>{this.props.stackline}</span>
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default StackTreeElementComponent;
+
+
+
+      /*<tr className={this.props.highlight ? styles.highlight : this.props.subdued && styles.subdued}>
+        <td>
           {this.props.samples ? (
             <div className={`${styles.pill} mdl-color-text--primary`}>
               <div className={styles.number}>{this.props.samples}</div>
@@ -48,9 +96,4 @@ class StackTreeElementComponent extends Component {
             </span>
           </div>
         </td>
-      </tr>
-    );
-  }
-}
-
-export default StackTreeElementComponent;
+      </tr>*/
