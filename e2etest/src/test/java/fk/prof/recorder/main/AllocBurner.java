81894e84b6d75b590aaed3bc9d72ab6bb9313730
Janmejay Singh <singh.janmejay@gmail.com>
2017-06-19 22:24:09 +0530

    Increase load on alloc-burner, it seems to finish too soon

--- e2etest/src/test/java/fk/prof/recorder/main/AllocBurner.java
+++ e2etest/src/test/java/fk/prof/recorder/main/AllocBurner.java
@@ -6,7 +6,7 @@ package fk.prof.recorder.main;
 public class AllocBurner {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            GcBench.runBenchmark(200, 80, GcBench.kArraySize, GcBench.kMinTreeDepth, GcBench.kMaxTreeDepth);
+            GcBench.runBenchmark(200, 120, GcBench.kArraySize, GcBench.kMinTreeDepth, GcBench.kMaxTreeDepth);
         }
     }
 }
